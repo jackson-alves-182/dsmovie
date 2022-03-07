bb1e788138bd596607ec18129052cf7eb3328bd8
Jackson Alves <jack182@outlook.com.br>
2022-03-07 16:11:14 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
-    <h1> Página Principal</h1>
+    <Navbar />
   );
 }
 
