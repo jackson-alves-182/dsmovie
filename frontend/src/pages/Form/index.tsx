5223781ebce68dd700d519ab8b6e2e6c09cdd3a1
Jackson Alves <jack182@outlook.com.br>
2022-03-08 12:14:08 -0300

    Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './style.css'
 
 function Form() {
@@ -36,7 +37,9 @@ function Form() {
             </button>
           </div>
         </form>
+        <Link to="/">
         <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        </Link>
       </div>
     </div>
   );
